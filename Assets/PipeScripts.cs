b6c51ab86b821103030fd53746b5e6152243e7a2
Thiago Farias <110398840+ThiagoFarias80@users.noreply.github.com>
2024-02-18 20:49:38 -0300

    Game Updates
    Add Pipes
    Spawners
    E etc

---
+++ Assets/PipeScripts.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeScripts : MonoBehaviour
+{
+
+    public float moveSpeed = 5;
+    public float deadzone = -23; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        if (transform.position.x < deadzone)
+        {
+            Debug.Log("Pipe Deleted");
+            Destroy(gameObject);
+        }
+    }
+}
